03661009eeb5fa0305b23e65940b5deda1c315dd
Binta001 <80017600+Binta001@users.noreply.github.com>
2025-02-03 13:45:49 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,63 @@
 # environment Variables
-variable "region" {}
-variable "project_name" {}
-variable "environment" {}
+variable "region" {
+default = "us-east-1"
+description = "my_region"
+type  = "string"
+}
+variable "project_name" {
+default = "terraformmodule"
+description = "my_module"
+type  = "string"
+}
+variable "environment" {
+default = "test_env"
+description = "my_Env"
+type  = "string"
+}
 
 # vpc variable
-variable "vpc_cidr" {}
-variable "public_subnet_az1_cidr" {}
-variable "public_subnet_az2_cidr" {}
-variable "private_app_subnet_az1_cidr" {}
-variable "private_app_subnet_az2_cidr" {}
-variable "private_data_subnet_az1_cidr" {}
-variable "private_data_subnet_az2_cidr" {}
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR Block"
+  type        = string
+
+}
+
+variable "public_subnet_az1_cidr" {
+  default     = "10.0.0.0/24"
+  description = "Public Subnet 1 CIDR Block"
+  type        = string
+
+}
+
+variable "public_subnet_az2_cidr" {
+  default     = "10.0.1.0/24"
+  description = "Public Subnet 2 CIDR Block"
+  type        = string
+
+}
+
+variable "private_app_subnet_az1_cidr" {
+  default     = "10.0.2.0/24"
+  description = "Private Subnet 1 CIDR Block"
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  default     = "10.0.3.0/24"
+  description = "Private Subnet 2 CIDR Block"
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  default     = "10.0.4.0/24"
+  description = "Private Subnet 3 CIDR Block"
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  default     = "10.0.5.0/24"
+  description = "Private Subnet 4 CIDR Block"
+  type        = string
+}
+  
